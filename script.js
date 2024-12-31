83eb874f64ad76537465d73dc9e0560b4822189c
Salsa <146612164+SalsaTriana@users.noreply.github.com>
2025-01-01 00:53:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Optionally you can add some dynamic behavior to the letter animation
+const music = document.getElementById("music");
+
+music.addEventListener("play", function () {
+    console.log("Lagu mulai diputar.");
+});
